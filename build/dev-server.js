d38b815327e89e5aaa755922fb7ee64526378e56
junhuaNing <2239127601@qq.com>
2017-10-14 23:54:17 +0800

    update

--- build/dev-server.js
+++ build/dev-server.js
@@ -62,7 +62,7 @@ app.use(devMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:' + port
+var uri = 'http://114.67.145.209:' + port
 
 var _resolve
 var readyPromise = new Promise(resolve => {
